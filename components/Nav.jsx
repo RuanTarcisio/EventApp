f2783b6bbcc81dfa8feac11986abec5368040a98
Ruan Tarcisio <ruantarciisio@gmail.com>
2025-08-08 17:29:29 -0300

    search page

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+
+const links = [
+  { name: "Home", path: "/" },
+  { name: "Nossos Eventos", path: "/our-events" },
+];
 
 const Nav = () => {
+  const pathname = usePathname();
   return (
-    <div>
-      Nav
-    </div>
-  )
-}
+    <nav className="flex gap-[36px]">
+      {links.map((link, index) => {
+        return (
+          <Link
+            href={link.path}
+            key={index}
+            className={`${link.path === pathname && "text-accent"}`}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
